49d2f44acc447cf6b2f88a060eb203b6bcdf362e
sadanand_01 <sadanand00101@gmail.com>
2026-08-19 21:50:29 +0530

    Create main.py

---
+++ Day7/main.py
@@ -0,0 +1,14 @@
+for i in range(5):
+ print("Hi")
+
+for i in range(4,9):
+    print(i)
+    
+for i in range(4,11,2):#(start,stop,increment)
+    print(i)
+
+count = 1  #variable
+while count <5:
+    print(count)
+    count+=1
+    
