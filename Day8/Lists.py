33b99b1658378dca0c20d4ce099cc9df8443795d
sadanand_01 <sadanand00101@gmail.com>
2026-08-19 21:50:53 +0530

    Create Lists.py

---
+++ Day8/Lists.py
@@ -0,0 +1,11 @@
+# list => change =[]
+#tuple =>cant change  =()
+
+list=[10,20,30,"hola",True]
+# print(list[-4])
+#methods = append,remove,len
+list.remove("hola")
+# print(len(list))
+
+tuple=(10,20,30)
+print(tuple[1])
