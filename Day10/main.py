2a16075a8c627f9e6821ea1b7bb5fb9b6917145a
sadanand_01 <sadanand00101@gmail.com>
2026-08-19 21:49:47 +0530

    Create main.py

---
+++ Day10/main.py
@@ -0,0 +1,23 @@
+# age = input("enter age")
+# print(age)
+total = 0
+numbers={10,20,30,40,50}
+
+for number in numbers:
+    total+=number
+    
+print(total)
+    
+obj={
+    "name":"akash",
+    "age":20,
+    "clg":"BEC"
+}
+
+for val1,val2 in obj.items():
+    print(val1,val2)
+
+for a in range(2):
+    for b in range(2):
+        print(a,b)
+
