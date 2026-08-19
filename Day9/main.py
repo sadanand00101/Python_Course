8f24b654a500d514933027f5a74b146a2bdce3ee
sadanand_01 <sadanand00101@gmail.com>
2026-08-19 21:51:00 +0530

    Create main.py

---
+++ Day9/main.py
@@ -0,0 +1,30 @@
+#dict 
+#set
+obj={
+    "name":"akash",
+    "age":20,
+    "clg":"BEC"
+}
+
+print(obj["name"])
+#update
+obj["age"]=21
+print(obj["age"])
+#new key-value
+obj["hobby"]="time pass"
+print(obj)
+#del
+del obj["hobby"]
+print(obj)
+#keys
+print(obj.keys())
+print(obj.values())
+
+
+#set
+
+abc={10,20,30,40,10,20}
+print(set)
+
+
+
